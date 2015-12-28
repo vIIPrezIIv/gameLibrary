bb2e86a610845214b83fc4d7d65105d66676a012
Real Ortelli <realortelli@gmail.com>
2015-12-27 21:36:04 -0500

    Update gameScript.sql
    Fixed formatting.

--- gameScript.sql
+++ gameScript.sql
@@ -3,37 +3,37 @@ CREATE DATABASE GameLibrary;
 USE GameLibrary;
 
 CREATE TABLE gameName (
-	gameId INT PRIMARY KEY AUTO_INCREMENT,
+    gameId INT PRIMARY KEY AUTO_INCREMENT,
     gameName VARCHAR(45),
     releaseYear DATE,
     dateAdded DATE
 );
 
 CREATE TABLE consolePc (
-	consolePcId INT PRIMARY KEY AUTO_INCREMENT,
+    consolePcId INT PRIMARY KEY AUTO_INCREMENT,
     gameId INT,
     consolePcName VARCHAR(25),
-		FOREIGN KEY (gameId)
-			REFERENCES gameName(gameId)
+	FOREIGN KEY (gameId)
+		REFERENCES gameName(gameId)
 );
 
 CREATE TABLE special (
-	specId INT PRIMARY KEY AUTO_INCREMENT,
+    specId INT PRIMARY KEY AUTO_INCREMENT,
     gameId INT,
     edition ENUM("Collectors Edition", "Special Edition", "Deluxe Edition", "Standard Edition"),
-		FOREIGN KEY (gameId)
-			REFERENCES gameName(gameId)
+	FOREIGN KEY (gameId)
+		REFERENCES gameName(gameId)
 );
 
 CREATE TABLE gameCover (
-	gameCoverId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    gameCoverId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     gameId INT NOT NULL,
-	name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     type VARCHAR(30) NOT NULL,
-	size INT NOT NULL,
-	content LONGBLOB NOT NULL,
-		FOREIGN KEY (gameId)
-			REFERENCES gameName(gameId)
+    size INT NOT NULL,
+    content LONGBLOB NOT NULL,
+	FOREIGN KEY (gameId)
+		REFERENCES gameName(gameId)
 );
 
 
