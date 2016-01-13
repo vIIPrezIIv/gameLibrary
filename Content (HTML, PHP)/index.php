bd48410134d5282a0eb9e695b6687e523c64448a
Graham <grahamstaveleyberry@gmail.com>
2016-01-13 16:52:59 -0500

    CSS Changes To Index.php
    Add Css to index.php

--- Content (HTML, PHP)/index.php
+++ Content (HTML, PHP)/index.php
@@ -6,49 +6,85 @@
         <title>gameInterface</title>
     </head>
     <body>
-        <div class="centerStyle">
-            <div class="insertGameStyle">
-                <form action ="insertGame.php" method="GET">
-                    <p>
-                        <input type ="submit" value ="Add a Game"/>
-                    </p> 
-                </form>
-            </div>
-            <div class="showGamesStyle">
-                <form action ="showGames.php" method="GET">
-                    <p>
-                        <input type ="submit" value ="Show Games"/>
-                    </p> 
-                </form>
-            </div>
-            <div class="returnGameStyle">
-                <form action ="returnGame.php" method="GET">
-                    <p>
-                        <label>Enter Game Name</label>
-                        <input type ='text' name ="searchName" required ="true" title ="Must enter a game name"/>
-                    </p>
-                    <p>
-                        <label>Enter Platform</label>
-                        <input type ='text' name ="searchPlatform" required ="true" title ="Must enter a platform"/>
-                    </p>
-                    <p>
-                        <input type ="submit" value ="Search"/>
-                    </p> 
-                </form>
-            </div>
-            <div class="insertCover">
-                <form action ="insertCover.php" method="POST" enctype="multipart/form-data">
-                    <p>
-                        <label>Game Name</label>
-                        <input type ='text' name ="gameCoverName" required ="true" title ="Must enter a game name for cover"/>
-                    </p>
-                    <p>
-                        <input type="hidden" name ="MAX_FILE_SIZE" value="2000000">
-                        <input name="userfile" type ="file"> 
-                        <input name="upload" type ="submit" value="Upload">
-                    </p>
-                </form>
-            </div>
-        </div>
+		<div class='pageHeader'>
+			Game Library
+		</div>
+		<div id='navBar'>
+			<div id='btnContainer'>
+				<div class='menuBtn'><a href='insertGame.php' class='menuSel'>Add Game</a></div>
+				<div class='menuBtn'><a href='showGames.php' class='menuSel'>Show Games</a></div>
+				<div class='menuBtn'><a href='returnGame.php' class='menuSel'>Search Games</a></div>
+				<div class='menuBtn'><a href='' class='menuSel'>Edit Game</a></div>
+				<!--<div class='menuBtn'>Add Game Cover</div>-->
+			</div>
+		</div>
+		
+		<div id='leftCol'>
+			<div class="returnGameStyle">
+				<h2>Search for Game</h2>
+				<form action ="returnGame.php" method="GET">
+					<label for='gameName'>Enter Game Name</label>
+					<input type ='text' name ="searchName" id='gameName' required ="true" title ="Must enter a game name"/><BR/>
+					<label for='platformName' >Enter Platform</label>
+					<input type ='text' name ="searchPlatform" id='platformName' required ="true" title ="Must enter a platform"/>
+					<input type ="submit" class='largeSubmit' value ="Search"/>					
+				</form>
+			</div>
+			<hr>
+			<div class='recentlyAdded'>
+				<h2>Recently Added</h2>
+				<div class='recentlyAdded_rst'>
+				</div>
+			</div>
+		</div>
+		<div id='rightCol'>
+			<div id='newFeed'>
+				<h1>Game News</h1>
+				<div id='newsContainer'>
+				</div>
+			</div>
+			<div id='featuredGame'>
+				<h1>Featured Game</h1>
+				<hr>
+				<h3 class='gameTitle'>Insert Game Name Here</h3>
+				<div class='quickInfo'>
+					<div class='imgHolder'>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--
+		<div class="showGamesStyle">
+				
+				<form action ="showGames.php" method="GET">
+					<p>
+						<input type ="submit" value ="Show Games"/>
+					</p> 
+				</form>
+			</div>
+		<div class="insertGameStyle">
+				<form action ="insertGame.php" method="GET">
+					<p>
+						<input type ="submit" value ="Add a Game"/>
+					</p> 
+				</form>
+			</div>
+		
+		
+		<div class="insertCover">
+			<form action ="insertCover.php" method="POST" enctype="multipart/form-data">
+				<p>
+					<label>Game Name</label>
+					<input type ='text' name ="gameCoverName" required ="true" title ="Must enter a game name for cover"/>
+				</p>
+				<p>
+					<input type="hidden" name ="MAX_FILE_SIZE" value="2000000">
+					<input name="userfile" type ="file"> 
+					<input name="upload" type ="submit" value="Upload">
+				</p>
+			</form>
+		</div>
+		-->
     </body>
 </html>
